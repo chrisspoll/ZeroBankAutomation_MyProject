7a395ac96c997926be72d2b2dd14ecfbe1d266ce
chrisspoll <sevin_b@yahoo.com>
2021-06-20 16:41:47 +0200

    4

--- src/test/java/com/zerobank/pages/CheckoutPage.java
+++ src/test/java/com/zerobank/pages/CheckoutPage.java
@@ -9,5 +9,6 @@ public class CheckoutPage {
         System.out.println("Hello");
         System.out.println("Hello2");
         System.out.println("Hello3");
+        System.out.println("Hello4");
     }
 }
