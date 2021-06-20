11d96745c622ba79fd5230e0d6fd859e7f7efeac
bilal <62038377+chrisspoll@users.noreply.github.com>
2021-06-20 14:54:22 +0200

    Update AccountActivityPage.java

--- src/test/java/com/zerobank/pages/AccountActivityPage.java
+++ src/test/java/com/zerobank/pages/AccountActivityPage.java
@@ -4,7 +4,6 @@ import com.zerobank.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import java.util.List;
 
 public class AccountActivityPage extends BasePage{
